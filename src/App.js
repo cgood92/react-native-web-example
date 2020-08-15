fb1d2e3e2d07fd263a921d740d246bae4e8d0c1f
Clint Goodman <clintgoodman@workfront.com>
2020-08-15 12:24:29 -0600

    feat: adding platform specific extensions

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import {Button, Provider, Toast} from '@ant-design/react-native';
 import {NativeRouter, Route, Switch} from 'react-router-native';
+import TextArea from './textarea';
 
 export default function App() {
   return (
@@ -18,6 +19,7 @@ export default function App() {
                 type="primary">
                 This is a button
               </Button>
+              <TextArea />
             </View>
           </Route>
         </Switch>
---
+++ src/textarea.native.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {TextareaItem} from '@ant-design/react-native';
+
+export default function TextArea() {
+  return <TextareaItem />;
+}
