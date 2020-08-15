7c2b07635ff77764f4f299b40e2a80691e643676
Clint Goodman <clintgoodman@workfront.com>
2020-08-15 11:35:29 -0600

    feat: add ant-design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {Button, Provider, Toast} from '@ant-design/react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello world!</Text>
-    </View>
+    <Provider>
+      <View>
+        <Text>Hello world!</Text>
+        <Button
+          onPress={() => Toast.info('Button was clicked!')}
+          style={{flex: 1}}
+          type="primary">
+          This is a button
+        </Button>
+      </View>
+    </Provider>
   );
 }
